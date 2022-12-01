63bbfa8ee32bd296dcc9ad260570dbf6e1a5e694
Márton Braun <braunmarci@gmail.com>
2022-12-01 09:31:05 +0100

    Add day 1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,60 @@
+import java.util.TreeSet
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var max = 0
+        var sum = 0
+
+        fun checkAndUpdate() {
+            if (sum > max) {
+                max = sum
+            }
+            sum = 0
+        }
+
+        for (line in input) {
+            if (line.isEmpty()) {
+                checkAndUpdate()
+                continue
+            }
+            sum += line.toInt()
+        }
+        checkAndUpdate()
+
+        return max
+    }
+
+    fun <T> List<T>.split(predicate: (T) -> Boolean): List<List<T>> {
+        val list = mutableListOf<MutableList<T>>()
+
+        var subList = mutableListOf<T>()
+        forEach { element ->
+            when {
+                predicate(element) -> {
+                    list += subList
+                    subList = mutableListOf()
+                }
+                else -> subList += element
+            }
+        }
+        list += subList
+
+        return list
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val set = TreeSet<Int>(reverseOrder())
+
+        input.split(String::isEmpty)
+            .map { elfInventory -> elfInventory.sumOf(String::toInt) }
+            .forEach(set::add)
+
+        return set.take(3).sum()
     }
 
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
