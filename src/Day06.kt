0ea5fc420b08e0453b440820277c9dc5863e3a88
Márton Braun <braunmarci@gmail.com>
2022-12-06 08:54:02 +0100

    Add day 6 solution

---
+++ src/Day06.kt
@@ -0,0 +1,16 @@
+fun main() {
+    fun findUniqueMarker(str: String, n: Int) =
+        str.windowed(n).indexOfFirst { lastN -> lastN.toSet().size == n } + n
+    //  str.windowed(n).takeWhile { lastN -> lastN.toSet().size != n }.size + n
+
+    fun part1(testInput: String) = findUniqueMarker(testInput, 4)
+    fun part2(testInput: String) = findUniqueMarker(testInput, 14)
+
+    val testInput = readInput("Day06_test").first()
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 19)
+
+    val input = readInput("Day06").first()
+    println(part1(input))
+    println(part2(input))
+}
